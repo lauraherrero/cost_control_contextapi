1a50604547f904f72b3019d3942c66c019f6346a
Laura Herrero Poza <lherrero@nextdigital.es>
2024-05-13 18:03:36 +0200

    Add graphic and reset button component

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,6 +1,18 @@
 
 export const BudgetTracker = () => {
   return (
-    <div>BudgetTracker</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+      <div className="flex justify-center">
+        <img src="/public/grafico.jpg" alt="Gráfica de gastos" />
+      </div>
+      <div className="flex flex-col justify-center items-center gap-8">
+        <button 
+          type="button"
+          className="bg-pink-600 w-full p-2 text-white uppercase font-bold"
+        >
+          Resetear App
+        </button>
+      </div>
+    </div>
   )
 }
